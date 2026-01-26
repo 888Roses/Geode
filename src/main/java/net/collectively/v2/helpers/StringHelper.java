e5b978dec2aa5ed8e6f237e56ab18d8daa8c305d
Rosenoire <skillwarzaking@gmail.com>
2026-01-26 12:09:55 +0100

    Added identifier to human-readable name String utilities.

--- src/main/java/net/collectively/v2/helpers/StringHelper.java
+++ src/main/java/net/collectively/v2/helpers/StringHelper.java
@@ -1,6 +1,12 @@
 package net.collectively.v2.helpers;
 
+import net.minecraft.util.Identifier;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /// Collection of utilities regarding `String` and texts.
 @SuppressWarnings("unused")
@@ -21,5 +27,40 @@ public interface StringHelper {
     static int countTailing(String string, Predicate<Character> character) {
         for (int i = string.length() - 1; i >= 0; i--) if (character.test(string.charAt(i))) return i;
         return 0;
+    }
+
+    /// Takes in an identifier-like `String` (lowercase only, no whitespace, words delimited by `_` characters) and
+    /// transforms it into a human-readable name. For example:
+    /// ```java
+    /// String identifier = "example_identifier";
+    /// System.out.println(StringHelper.toHumanName(identifier));
+    /// 
+    /// // [Console]
+    /// // Example identifier
+    /// ```
+    /// 
+    /// @param identifier Identifier pattern `String` to transform into a human-readable text.
+    /// @return           The human-readable text.
+    /// @apiNote          The given identifier `String` is not checked. You are responsible for making sure it respects the
+    ///                   aforementioned identifier characteristics.
+    /// @see #toHumanName(Identifier) 
+    static String toHumanName(String identifier) {
+        return Arrays.stream(identifier.split("_")).map(StringUtils::capitalize).collect(Collectors.joining(" "));
+    }
+    
+    /// Takes in an [identifier][Identifier] and returns a human-readable name created using its [path][Identifier#path].
+    /// For example:
+    /// ```java
+    /// Identifier identifier = Identifier.of("example_mod", "op_item"):
+    /// System.out.println(StringHelper.toHumanName(identifier));
+    /// 
+    /// // [Console]
+    /// // Op Item
+    /// ```
+    /// @param identifier The identifier whose path is going to be transformed into a human-readable text.
+    /// @return The human-readable text.
+    /// @see #toHumanName(String) 
+    static String toHumanName(@NotNull Identifier identifier) {
+        return toHumanName(identifier.getPath());
     }
 }
