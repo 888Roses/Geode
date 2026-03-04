797cbc2b92f450d926017b6b81ce169087281d8b
rosenoire <162016745+888Roses@users.noreply.github.com>
2026-03-04 10:50:13 +0100

    Remaking DataGen

--- src/main/java/net/collectively/geode/GeodeClient.java
+++ src/main/java/net/collectively/geode/GeodeClient.java
@@ -1,10 +1,15 @@
 package net.collectively.geode;
 
 import net.collectively.geode.registration.ClientRegisterer;
+import net.minecraft.util.Identifier;
 
 @SuppressWarnings("unused")
 public class GeodeClient implements ClientRegisterer {
     private final String linkedModId;
+
+    public Identifier id(String identifier) {
+        return Identifier.of(linkedModId, identifier);
+    }
 
     /// Creates a new [GeodeClient] instance to be used for the given mod.
     public static GeodeClient create(String linkedModId) {
