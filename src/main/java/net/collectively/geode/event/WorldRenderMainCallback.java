91ef943b1894ae5e242f4cd4a49714db11891ea7
rosenoire <162016745+888Roses@users.noreply.github.com>
2026-03-14 09:42:23 +0100

    World Render Callback

---
+++ src/main/java/net/collectively/geode/event/WorldRenderMainCallback.java
@@ -0,0 +1,79 @@
+package net.collectively.geode.event;
+
+import com.mojang.blaze3d.buffers.GpuBufferSlice;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.client.render.FrameGraphBuilder;
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.client.render.state.WorldRenderState;
+import net.minecraft.util.profiler.Profiler;
+import org.joml.Matrix4f;
+
+@Environment(EnvType.CLIENT)
+public interface WorldRenderMainCallback {
+    Event<WorldRenderMainCallback> BEFORE = EventFactory.createArrayBacked(
+            WorldRenderMainCallback.class,
+            (listeners) -> (
+                    FrameGraphBuilder frameGraphBuilder,
+                    Frustum frustum,
+                    Matrix4f posMatrix,
+                    GpuBufferSlice fogBuffer,
+                    boolean renderBlockOutline,
+                    WorldRenderState state,
+                    RenderTickCounter tickCounter,
+                    Profiler profiler
+            ) -> {
+                for (WorldRenderMainCallback listener : listeners) {
+                    listener.renderMain(
+                            frameGraphBuilder,
+                            frustum,
+                            posMatrix,
+                            fogBuffer,
+                            renderBlockOutline,
+                            state,
+                            tickCounter,
+                            profiler
+                    );
+                }
+            }
+    );
+
+    Event<WorldRenderMainCallback> AFTER = EventFactory.createArrayBacked(
+            WorldRenderMainCallback.class,
+            (listeners) -> (
+                    FrameGraphBuilder frameGraphBuilder,
+                    Frustum frustum,
+                    Matrix4f posMatrix,
+                    GpuBufferSlice fogBuffer,
+                    boolean renderBlockOutline,
+                    WorldRenderState state,
+                    RenderTickCounter tickCounter,
+                    Profiler profiler
+            ) -> {
+                for (WorldRenderMainCallback listener : listeners) {
+                    listener.renderMain(
+                            frameGraphBuilder,
+                            frustum,
+                            posMatrix,
+                            fogBuffer,
+                            renderBlockOutline,
+                            state,
+                            tickCounter,
+                            profiler
+                    );
+                }
+            }
+    );
+
+    void renderMain(FrameGraphBuilder frameGraphBuilder,
+                    Frustum frustum,
+                    Matrix4f posMatrix,
+                    GpuBufferSlice fogBuffer,
+                    boolean renderBlockOutline,
+                    WorldRenderState state,
+                    RenderTickCounter tickCounter,
+                    Profiler profiler);
+}
